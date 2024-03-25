864ae7273a7248301b8d07875c0e232f903ebb0f
Cesareuh <mathieumatique@hotmail.fr>
2024-03-25 15:04:44 +0100

    vps pour gestions mdp

--- config/scripts/sync_passwd.sh
+++ config/scripts/sync_passwd.sh
@@ -4,5 +4,5 @@ if [ ! -d $HOME/.passwd ]; then
 	mkdir $HOME/.passwd
 fi
 
-rsync -avu $HOME/.passwd/Passwords.kdbx e6436u@webetu.iutnc.univ-lorraine.fr:.passwd/Password.kdbx
-rsync -avu e6436u@webetu.iutnc.univ-lorraine.fr:.passwd/Password.kdbx $HOME/.passwd/Passwords.kdbx
+rsync -avu $HOME/.passwd/Passwords.kdbx mathieu@20.19.88.144:.passwd/Password.kdbx
+rsync -avu mathieu@20.19.88.144:.passwd/Password.kdbx $HOME/.passwd/Passwords.kdbx
