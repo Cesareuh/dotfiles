791a1ab358f7873ee120f3b730b22983a0a04b63
Cesareuh <mathieumatique@hotmail.fr>
2024-04-03 17:08:57 +0200

    renommage de la base de données des mdp sur le VPS

--- config/scripts/sync_passwd.sh
+++ config/scripts/sync_passwd.sh
@@ -4,5 +4,5 @@ if [ ! -d $HOME/.passwd ]; then
 	mkdir $HOME/.passwd
 fi
 
-rsync -avu $HOME/.passwd/Passwords.kdbx mathieu@20.19.88.144:.passwd/Password.kdbx
-rsync -avu mathieu@20.19.88.144:.passwd/Password.kdbx $HOME/.passwd/Passwords.kdbx
+rsync -avu $HOME/.passwd/Passwords.kdbx mathieu@20.19.88.144:.passwd/Passwords.kdbx
+rsync -avu mathieu@20.19.88.144:.passwd/Passwords.kdbx $HOME/.passwd/Passwords.kdbx
