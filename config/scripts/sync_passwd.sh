6a44675b2d36501e9eee112e3c35661edfc443c9
Cesareuh <mathieumatique@hotmail.fr>
2024-06-02 01:26:38 +0200

    quelques changements...

--- config/scripts/sync_passwd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d $HOME/.passwd ]; then
-	mkdir $HOME/.passwd
-fi
-
-rsync -avu $HOME/.passwd/Passwords.kdbx mathieu@20.19.88.144:.passwd/Passwords.kdbx
-rsync -avu mathieu@20.19.88.144:.passwd/Passwords.kdbx $HOME/.passwd/Passwords.kdbx
