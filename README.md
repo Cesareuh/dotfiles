53d5851a12ea8b79fb5cc01ea7f92640397ecedc
Cesareuh <mathieumatique@hotmail.fr>
2025-03-01 01:07:30 +0100

    installation auto quasi finie

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # dotfiles
-## Install
-To install : run the ```config.sh``` script   
-YOUR DEFAULT CONFIG FILES WILL BE REMOVED WITH THIS SCRIPT
+## Description
+This repo contains my dotfiles and can vary a lot over time as my mood changes.   
+It is a basic setup and can be helpful for beginner who wants to get started on linux but don't know how to start.   
+Feel free to copy my files and use it as you want.
+
+ - 🎨 Compositor : [Hyprland](https://hyprland.org)
+    -  🧍 Idle daemon : [hypridle](https://github.com/hyprwm/hypridle)
+    - 🔒 Lock screen : [hyprlock](https://github.com/hyprwm/hyprlock/)
+    - 🖼️ Wallpaper : [hyprpaper](https://github.com/hyprwm/hyprpaper)
+ - ⚙️ Bar : [waybar](https://github.com/Alexays/Waybar)
+ - 💻 Terminal : [kitty](https://sw.kovidgoyal.net/kitty)
+ - 🚀 Launcher : [fuzzel](https://codeberg.org/dnkl/fuzzel)
+ - 🌈 Theme : [tokyo-night-storm](https://github.com/tokyo-night/tokyo-night-vscode-theme)
+ - 🔔 Notification daemon : [mako](https://github.com/emersion/mako)
+## Automatic install
+The ```config.sh``` script will help you install my dotfiles but use it only if you **KNOW WHAT YOU DO**     
+### Warning
+ - It will **REMOVE** config files that are in conflict
+ - It can install some **UNWANTED PACKAGES**
+ - Some changes are **IRREVERSIBLE**
+
+### Safe install
+You can use it without any risk if you are running it on a new user
+
+## TODO
+ - hypridle and hyprlock not working
--- config.sh
+++ config.sh
@@ -1,14 +1,128 @@
 #!/bin/bash
 
-cd config
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+RED="\e[1;31m"
+RESET="\e[0m"
 
-for i in *; do
-	if [ -d ~/.config/$i ]; then
-		rm -r ~/.config/$i
-		echo "delete : $i"
+echo "This script is designed to help the creator of these dotfiles to update and fast setup his config on a new computer"
+echo -e "\tYou have to execute it in the repertory you found it"
+echo -e "\tIt will install packages needed for the config"
+echo -e "\tIt will delete concurent config files"
+echo -e "\tMake sure to$RED execute as a new user$RESET or$RED know what you do$RESET before executing this"
+echo -ne "$YELLOW"
+echo -e "If you don't want consequences you can just browse my config files and copy the one you like$RESET"
+echo -ne "$RED"
+read -p "DO YOU REALLY WANT TO EXECUTE IT ? [y/N] " answer
+echo -ne "$RESET"
+
+
+if [ -z $answer ] || [ $answer != "y" ]; then
+	exit 0
+fi
+
+dir=$PWD
+
+read -p "Install yay ? [y/N] " answer
+
+if [ -z $answer ]; then
+	answer="n"
+fi
+
+if [ $answer = "y" ]; then
+	echo -e "Installing yay"
+	sudo pacman -S --needed git base-devel
+
+	cd ~/
+
+	if [ ! -d git ]; then
+		mkdir git
 	fi
 
-	ln -s $PWD/$i ~/.config/$i
-	echo "$i added in ~/.config"
+	cd git
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+	echo "Yay installed"
+fi
 
+
+
+read -p "Install packages used by these dotfiles (installing with yay) ? [y/N] : " answer
+
+if [ -z $answer ]; then
+	answer="n"
+fi
+
+if [ $answer = "y" ]; then
+	yay -S zsh wget hyprland-git hyprutils-git hyprland-protocols-git hypridle-git hyprlock-git hyprshot-git hyprpaper-git kitty uwsm qt5ct gtk4 gtk3 papirus-icon-theme waybar-cava-git udiskie fuzzel mako ttf-hack-nerd noto-fonts-cjk noto-fonts-emoji
+fi
+
+read -p "Set default shell to zsh [y/N] : " answer
+
+if [ -z $answer ]; then
+	answer="n"
+fi
+
+if [ $answer = "y" ]; then
+	chsh -s /usr/bin/zsh
+fi
+
+read -p "Install Oh My Zsh ? [y/N] : " answer
+if [ -z $answer ]; then
+	answer="n"
+fi
+if [ $answer = "y" ]; then
+	# Bloque pendant l'installation, il faut juste faire CTRL+D après l'installation
+	sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+	read -p "Install Powerlevel10k ? [y/N] : " answer
+	if [ -z $answer ]; then
+		answer="n"
+	fi
+	if [ $answer = "y" ]; then
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+	fi
+fi
+
+echo "Installing config files"
+
+cd $dir/home
+
+# Get hidden files
+shopt -s dotglob
+
+for i in *; do
+	echo $i
+	if [ $i = "config" ]; then
+		cd config
+		for j in *; do
+			if [ -f ~/$i ] || [ -d ~/.config/$j ]; then
+				rm -r ~/.config/$j
+				echo "Deleting link to $j"
+			fi
+
+			ln -s $PWD/$j ~/.config/$j
+			echo "Link to $j added in ~/.config"
+		done
+		cd ../
+	else
+		if [ -f ~/$i ] || [ -d ~/$i ]; then
+			rm -r ~/$i
+			echo "Deleting link to $i"
+		fi
+
+		ln -s $PWD/$i ~/$i
+		echo "Link to $i added in ~/"
+	fi
 done
+
+shopt -u dotglob
+
+echo "Setting up Papirus icons"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+echo "Setting up Tokyonight dark storm gtk theme"
+gsettings set org.gnome.desktop.interface gtk-theme "Tokyonight-Dark-Storm"
+
+systemctl --user enable --now hyprpaper
+systemctl --user enable --now hypridle
